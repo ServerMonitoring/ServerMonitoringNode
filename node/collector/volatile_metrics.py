7c0549054163935c2dcb767020b5afeeaca0ad8d
GhostDragon <bk.if3@ya.ru>
2025-05-06 18:26:03 +0400

    Update volatile_metrics.py

--- node/collector/volatile_metrics.py
+++ node/collector/volatile_metrics.py
@@ -8,7 +8,8 @@ def get_volatile_gpu_metrics():
             "load_percent": round(gpu.load * 100, 2),
             "memory_used_MB": gpu.memoryUsed,
             "memoryFree": gpu.memoryFree,
-            "memory_used_percent": round(gpu.memoryUtil * 100, 2)
+            "memory_used_percent": round(gpu.memoryUtil * 100, 2),
+            "temperature": gpu.temperature
         }
     return gpus
 
